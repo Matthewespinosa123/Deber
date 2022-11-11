d734e49e8220ba6d6987be6feafa50346f8c199b
Matthewespinosa123 <116740310+Matthewespinosa123@users.noreply.github.com>
2022-11-11 10:34:20 -0500

    Add files via upload

---
+++ javascriptTestMatthewEspinosa/script.js
@@ -0,0 +1,6 @@
+console.log("La ventana fue abierta")
+function mainFunction(){  
+    console.log("texto"+mainFunction)
+    let printText=document.getElementById("mainElement")
+    alert("Ups! Nada que mostrar")        
+}
